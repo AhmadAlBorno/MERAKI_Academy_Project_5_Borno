e20812d596dcc8f41df241a6dc6593b4dee2b29f
AhmadMAlhyari <ahmad.m.alhiyari@gmail.com>
2025-12-23 23:38:50 +0300

    addnewCategor

--- Backend/MVC/controllers/categories.js
+++ Backend/MVC/controllers/categories.js
@@ -10,15 +10,37 @@ const addNewCategory = async (req, res) => {
       [name, description, imgsrc]
     );
     res.status(201).json({
-        success:true,
-        result:result.rows
-    })
+      success: true,
+      result: result.rows,
+    });
   } catch (err) {
     res.status(500).json({
-        success:false,
-        result : "Server error"
-    })   
+      success: false,
+      result: "Server error",
+    });
   }
 };
 
-module.exports = {addNewCategory}
+const getCategoryById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query(
+      `
+            SELECT name,imgsrc , description FROM categories WHERE id = $1 RETURNING *`,
+      [id]
+    );
+    res.status(200).json({
+        success:true,
+        result: result.rows
+    })
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// const updateCategoryById = async(req,res)=>{
+//     const {}
+// }
+
+module.exports = { addNewCategory,getCategoryById };
