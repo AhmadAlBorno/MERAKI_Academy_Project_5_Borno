2495930f05ed8d07a5f05cc093a0793526e42d2b
MahmoudQasm <m.alshiekhqasem@gmail.com>
2025-12-25 02:52:11 +0300

    corrected

--- Backend/MVC/routes/cart.js
+++ Backend/MVC/routes/cart.js
@@ -11,7 +11,7 @@ const cartRouter = express.Router();
 cartRouter.post("/", addToCart);
 cartRouter.put("/update", updateCart);
 cartRouter.delete("/delete", deleteFromCart);
-cartRouter.put("/checkou", checkout);
+cartRouter.put("/check", checkout);
 //========================
 
 module.exports = cartRouter;
